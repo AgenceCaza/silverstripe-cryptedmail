87f7d9b73420d5f1688d0542d57c8ef00e12aaca
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-01-28 11:28:37 -0500

    add support for extra dom
     (example@domain.tld.extratld)

--- _config.php
+++ _config.php
@@ -5,16 +5,27 @@ use SilverStripe\View\Parsers\ShortcodeParser;
 ShortcodeParser::get('default')->register('CryptedMail',
   function($arguments, $address, $parser, $shortcode) {
 
-  $email = $arguments['value'];
+	$extradom = "";
+
+	$email = $arguments['value'];
   $a1 = explode("@", $email);
   $username = $a1[0];
-  $dom = explode(".", $a1[1]);
+	$dom = explode(".", $a1[1]);
+
+	if (isset($dom[2])) {
+		$dom2 = $dom[2];
+
+		if (isset($dom2)) {
+			$extradom = ".".$dom2;
+		}
+
+	}
 
   return '
     <a href="#" class="cryptedmail"
        data-name="'.$username.'"
        data-domain="'.$dom[0].'"
-       data-tld="'.$dom[1].'"
+       data-tld="'.$dom[1].$extradom.'"
        onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain + \'.\' + this.dataset.tld; return false;">
      </a>';
 });
