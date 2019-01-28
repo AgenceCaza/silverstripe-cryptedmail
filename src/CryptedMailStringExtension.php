c22be57eac8024714fd9a3fdcd11638e05540c5a
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-01-28 11:30:50 -0500

    Add extra domain tld

--- src/CryptedMailStringExtension.php
+++ src/CryptedMailStringExtension.php
@@ -7,20 +7,31 @@ class CryptedMailStringExtension extends Extension {
 
 	public function CryptedMail() {
 
+	  $extradom = "";
+
 	  $email = $this->owner->value;
 	  $a1 = explode("@", $email);
 	  $username = $a1[0];
 	  $dom = explode(".", $a1[1]);
 
-		return DBField::create_field('HTMLVarchar','
-			<a href="#" class="cryptedmail"
-				 data-name="'.$username.'"
-				 data-domain="'.$dom[0].'"
-				 data-tld="'.$dom[1].'"
-				 onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain + \'.\' + this.dataset.tld; return false;">
-			 </a>'
-		 );
+	  if (isset($dom[2])) {
+	    $dom2 = $dom[2];
 
- }
+	    if (isset($dom2)) {
+	      $extradom = ".".$dom2;
+	    }
+
+	}
+
+	return DBField::create_field('HTMLVarchar','
+		<a href="#" class="cryptedmail"
+			 data-name="'.$username.'"
+			 data-domain="'.$dom[0].'"
+			 data-tld="'.$dom[1].$extradom.'"
+			 onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain + \'.\' + this.dataset.tld; return false;">
+		 </a>'
+	 );
+
+ 	}
 
 }
