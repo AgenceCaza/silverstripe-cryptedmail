d61c9350e1adce7d0ee9ae68cdfdf8709a7710d3
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-02-13 14:47:05 -0500

    Update CryptedMailStringExtension.php

--- src/CryptedMailStringExtension.php
+++ src/CryptedMailStringExtension.php
@@ -27,6 +27,32 @@ class CryptedMailStringExtension extends Extension {
 		 </a>'
 	 );
 
-  }
+  	}
+	
+	
+	public function CryptedMailNoTag() {
+
+		$extradom = "";
+
+		$email = $this->owner->value;
+		$a1 = explode("@", $email);
+		$username = $a1[0];
+		$dom = explode(".", $a1[1]);
+
+		if (isset($dom[2])) {
+				$extradom = ".".$dom[2];
+		}
+
+	return DBField::create_field('HTMLVarchar','
+		class="cryptedmailnotag"
+			 data-name="'.$username.'"
+			 data-domain="'.$dom[0].'"
+			 data-tld="'.$dom[1].$extradom.'"
+			 onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain + \'.\' + this.dataset.tld; return false;"'
+	 );
+
+	}
+	
+	
 
 }
