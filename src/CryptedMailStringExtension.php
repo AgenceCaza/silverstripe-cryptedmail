3b8c7f09c7614337378718788dae7ba44c81810e
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-04-25 09:10:07 -0400

    Update CryptedMailStringExtension.php

--- src/CryptedMailStringExtension.php
+++ src/CryptedMailStringExtension.php
@@ -7,39 +7,35 @@ class CryptedMailStringExtension extends Extension {
 
 	public function CryptedMail() {
 
-	  $extradom = "";
-
 	  $email = $this->owner->value;
-	  $a1 = explode("@", $email);
-	  $username = $a1[0];
-	  $domain = $a1[1];
+	  $part = explode("@", $email);
+	  $username = $part[0];
+	  $domain = $part[1];
 
-	return DBField::create_field('HTMLVarchar','
+	  return DBField::create_field('HTMLVarchar','
 		<a href="#" class="cryptedmail"
 			 data-name="'.$username.'"
 			 data-domain="'.$domain.'"
 			 onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain; return false;">
 		 </a>'
-	 );
+	   );
 
   	}
 	
 	
 	public function CryptedMailNoTag() {
 
-		$extradom = "";
-
 		$email = $this->owner->value;
-		$a1 = explode("@", $email);
-		$username = $a1[0];
-		$domain = $a1[1];
+		$part = explode("@", $email);
+		$username = $part[0];
+		$domain = $part[1];
 
-	return DBField::create_field('HTMLVarchar','
+	  return DBField::create_field('HTMLVarchar','
 		class="cryptedmailnotag"
 			 data-name="'.$username.'"
 			 data-domain="'.$domain.'"
 			 onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain; return false;"'
-	 );
+	   );
 
 	}
 	
