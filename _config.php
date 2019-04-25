ce6dd59fe399a5690b10fa68870ff01e7797ece1
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-04-25 09:09:20 -0400

    Update _config.php

--- _config.php
+++ _config.php
@@ -5,22 +5,15 @@ use SilverStripe\View\Parsers\ShortcodeParser;
 ShortcodeParser::get('default')->register('CryptedMail',
   function($arguments, $address, $parser, $shortcode) {
 
-	$extradom = "";
-
 	$email = $arguments['value'];
-  	$a1 = explode("@", $email);
-	$username = $a1[0];
-	$dom = explode(".", $a1[1]);
-
-	if (isset($dom[2])) {
-		$extradom = ".".$dom[2];
-	}
+  	$part = explode("@", $email);
+	$username = $part[0];
+	$domain = $part[1];
 
   return '
     <a href="#" class="cryptedmail"
        data-name="'.$username.'"
-       data-domain="'.$dom[0].'"
-       data-tld="'.$dom[1].$extradom.'"
-       onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain + \'.\' + this.dataset.tld; return false;">
+       data-domain="'.$domain.'"
+       onclick="window.location.href = \'mailto:\' + this.dataset.name + \'@\' + this.dataset.domain; return false;">
      </a>';
 });
